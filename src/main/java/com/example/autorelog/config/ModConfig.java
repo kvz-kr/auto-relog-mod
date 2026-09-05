9c26c34b0f4c686366014d609c09835013322504
kvz-kr <kylepro.michael@gmail.com>
2026-09-05 22:29:26 +0300

    Update ModConfig.java

--- src/main/java/com/example/autorelog/config/ModConfig.java
+++ src/main/java/com/example/autorelog/config/ModConfig.java
@@ -1,12 +1,47 @@
 package com.example.autorelog;
 
-public class ModConfig {
-    public static final ModConfig INSTANCE = new ModConfig();
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ModConfig {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final File CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("autorelog.json").toFile();
+
+    public static ModConfig INSTANCE = new ModConfig();
+
+    // Persistent Settings
     public boolean enabled = true;
     public double yThreshold = -5.0;
     public boolean autoReconnect = true;
-    public boolean isReconnecting = false;
     public int selectedSlot = 0;
-    public boolean shouldChangeSlotOnJoin = false;
+
+    // Session Runtime Flags (Do Not Save)
+    public transient boolean isReconnecting = false;
+    public transient boolean shouldChangeSlotOnJoin = false;
+
+    public static void load() {
+        if (CONFIG_FILE.exists()) {
+            try (FileReader reader = new FileReader(CONFIG_FILE)) {
+                INSTANCE = GSON.fromJson(reader, ModConfig.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            save(); // Save default values if config file doesn't exist yet
+        }
+    }
+
+    public static void save() {
+        try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
+            GSON.toJson(INSTANCE, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
