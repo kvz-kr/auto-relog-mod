56c208523342b00d44a7ea90c4408493ab6585a5
kvz-kr <kylepro.michael@gmail.com>
2026-09-06 12:55:37 +0300

    Update ConfigScreen.java

--- src/main/java/com/example/autorelog/gui/ConfigScreen.java
+++ src/main/java/com/example/autorelog/gui/ConfigScreen.java
@@ -5,8 +5,6 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
-import java.util.List;
-
 public class ConfigScreen extends Screen {
     private final Screen parent;
 
@@ -15,69 +13,56 @@ public class ConfigScreen extends Screen {
         this.parent = parent;
     }
 
-    private void drawBorder(DrawContext context, int x, int y, int width, int height, int color) {
-        context.fill(x, y, x + width, y + 1, color);
-        context.fill(x, y + height - 1, x + width, y + height, color);
-        context.fill(x, y, x + 1, y + height, color);
-        context.fill(x + width - 1, y, x + width, y + height, color);
-    }
-
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        // Dark translucent overlay
-        context.fill(0, 0, this.width, this.height, 0xBB0A0A0C);
+        context.fill(0, 0, this.width, this.height, 0xCC000000);
 
-        // Header Banner
-        context.fill(0, 0, this.width, 32, 0xFF121216);
-        context.fill(0, 31, this.width, 32, 0xFF00E676);
-        context.drawTextWithShadow(this.textRenderer, "KRYPTON", 16, 11, 0xFF00E676);
-        context.drawTextWithShadow(this.textRenderer, "v1.0.0 | Press ESC to save & exit", this.width - 180, 11, 0xFF888888);
+        int panelX = 30;
+        int panelY = 30;
+        int width = 220;
 
-        Module.Category[] categories = Module.Category.values();
-        List<AbstractModule> allModules = ModuleManager.INSTANCE.getModules();
+        SusChunkFinderModule mod = SusChunkFinderModule.INSTANCE;
 
-        int columnWidth = 120;
-        int cardHeight = 24;
-        int gapX = 14;
-        int startX = 20;
+        // Title Header
+        context.drawTextWithShadow(this.textRenderer, "SUS CHUNK FINDER", panelX, panelY, 0xFF55FFFF);
+        
+        // Main Toggle
+        int toggleColor = mod.isEnabled() ? 0xFF55FF55 : 0xFF555555;
+        context.fill(panelX + width - 20, panelY, panelX + width, panelY + 10, toggleColor);
 
-        for (int c = 0; c < categories.length; c++) {
-            Module.Category cat = categories[c];
-            int currentX = startX + c * (columnWidth + gapX);
-            int currentY = 48;
+        int y = panelY + 20;
 
-            // Category Header Box
-            context.fill(currentX, currentY, currentX + columnWidth, currentY + 20, 0xFF1A1A22);
-            drawBorder(context, currentX, currentY, columnWidth, 20, 0xFF2A2A36);
-            context.drawCenteredTextWithShadow(this.textRenderer, cat.getDisplayName().toUpperCase(), currentX + (columnWidth / 2), currentY + 6, 0xFF00E676);
+        // Simulation Distance
+        context.drawTextWithShadow(this.textRenderer, "SIMULATION DISTANCE   " + mod.simulationDistance, panelX, y, 0xFFFFFFFF);
+        y += 18;
 
-            currentY += 24;
+        // Sensitivity
+        context.drawTextWithShadow(this.textRenderer, "SENSITIVITY           " + mod.sensitivity, panelX, y, 0xFFFFFFFF);
+        y += 18;
 
-            // Render Modules
-            for (AbstractModule mod : allModules) {
-                if (mod.getCategory() == cat) {
-                    boolean enabled = mod.isEnabled();
-                    boolean hovered = mouseX >= currentX && mouseX <= currentX + columnWidth && mouseY >= currentY && mouseY <= currentY + cardHeight;
+        // Smart Adjustment Toggle
+        context.drawTextWithShadow(this.textRenderer, "[ " + (mod.smartAdjustment ? "X" : " ") + " ] SMART ADJUSTMENT", panelX, y, 0xFFCCCCCC);
+        y += 18;
 
-                    int bgColor = enabled ? (hovered ? 0xFF1B382B : 0xFF142B20) : (hovered ? 0xFF252530 : 0xFF16161E);
-                    int borderColor = enabled ? 0xFF00E676 : (hovered ? 0xFF444454 : 0xFF22222C);
-                    int textColor = enabled ? 0xFFFFFFFF : (hovered ? 0xFFCCCCCC : 0xFF777788);
+        // Alpha
+        context.drawTextWithShadow(this.textRenderer, "ALPHA                 " + mod.alpha, panelX, y, 0xFFFFFFFF);
+        y += 22;
 
-                    // Module Body
-                    context.fill(currentX, currentY, currentX + columnWidth, currentY + cardHeight, bgColor);
-                    drawBorder(context, currentX, currentY, columnWidth, cardHeight, borderColor);
+        // Block Toggles
+        mod.kelp = renderCheckbox(context, panelX, y, "KELP", mod.kelp, mouseX, mouseY); y += 16;
+        mod.caveVines = renderCheckbox(context, panelX, y, "CAVE VINES", mod.caveVines, mouseX, mouseY); y += 16;
+        mod.vines = renderCheckbox(context, panelX, y, "VINES", mod.vines, mouseX, mouseY); y += 16;
+        mod.amethyst = renderCheckbox(context, panelX, y, "AMETHYST", mod.amethyst, mouseX, mouseY); y += 16;
+        mod.bamboo = renderCheckbox(context, panelX, y, "BAMBOO", mod.bamboo, mouseX, mouseY); y += 16;
+        mod.cocoa = renderCheckbox(context, panelX, y, "COCOA", mod.cocoa, mouseX, mouseY); y += 16;
+        mod.beeNest = renderCheckbox(context, panelX, y, "BEE NEST", mod.beeNest, mouseX, mouseY); y += 16;
+        mod.rotatedDeepslate = renderCheckbox(context, panelX, y, "ROTATED DEEPSLATE", mod.rotatedDeepslate, mouseX, mouseY);
+    }
 
-                    // Module Name
-                    context.drawTextWithShadow(this.textRenderer, mod.getName(), currentX + 8, currentY + 8, textColor);
-
-                    // Status Indicator
-                    int indicatorColor = enabled ? 0xFF00E676 : 0xFF3A3A4A;
-                    context.fill(currentX + columnWidth - 14, currentY + 8, currentX + columnWidth - 6, currentY + 16, indicatorColor);
-
-                    currentY += cardHeight + 4;
-                }
-            }
-        }
+    private boolean renderCheckbox(DrawContext context, int x, int y, String label, boolean val, int mouseX, int mouseY) {
+        int color = val ? 0xFF55FFFF : 0xFF888888;
+        context.drawTextWithShadow(this.textRenderer, (val ? "[X] " : "[ ] ") + label, x, y, color);
+        return val;
     }
 
     @Override
@@ -85,30 +70,20 @@ public class ConfigScreen extends Screen {
         if (click.button() == 0) {
             double mouseX = click.x();
             double mouseY = click.y();
+            SusChunkFinderModule mod = SusChunkFinderModule.INSTANCE;
 
-            Module.Category[] categories = Module.Category.values();
-            List<AbstractModule> allModules = ModuleManager.INSTANCE.getModules();
+            int panelX = 30;
+            int y = 142; // Start of checkbox array
 
-            int columnWidth = 120;
-            int cardHeight = 24;
-            int gapX = 14;
-            int startX = 20;
-
-            for (int c = 0; c < categories.length; c++) {
-                Module.Category cat = categories[c];
-                int currentX = startX + c * (columnWidth + gapX);
-                int currentY = 72;
-
-                for (AbstractModule mod : allModules) {
-                    if (mod.getCategory() == cat) {
-                        if (mouseX >= currentX && mouseX <= currentX + columnWidth && mouseY >= currentY && mouseY <= currentY + cardHeight) {
-                            mod.toggle();
-                            return true;
-                        }
-                        currentY += cardHeight + 4;
-                    }
-                }
-            }
+            // Checkbox click bounds handler
+            if (mouseY >= y && mouseY <= y + 16) mod.kelp = !mod.kelp; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.caveVines = !mod.caveVines; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.vines = !mod.vines; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.amethyst = !mod.amethyst; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.bamboo = !mod.bamboo; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.cocoa = !mod.cocoa; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.beeNest = !mod.beeNest; y += 16;
+            if (mouseY >= y && mouseY <= y + 16) mod.rotatedDeepslate = !mod.rotatedDeepslate;
         }
         return super.mouseClicked(click, doubled);
     }
