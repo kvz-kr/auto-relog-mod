b573fe3bfae2d41a6eff022ab960afada9a60200
kvz-kr <kylepro.michael@gmail.com>
2026-09-06 12:09:45 +0300

    Update ConfigScreen.java

--- src/main/java/com/example/autorelog/gui/ConfigScreen.java
+++ src/main/java/com/example/autorelog/gui/ConfigScreen.java
@@ -19,7 +19,6 @@ public class ConfigScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
 
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 15, 0xFFFFFFFF);
@@ -102,7 +101,7 @@ public class ConfigScreen extends Screen {
                 }
             }
         }
-        return super.mouseClicked(mouseX, mouseY, button);
+        return false;
     }
 
     @Override
